e6da6c495d0ae1d8154ba56060da2da89a9e2388
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-16 21:12:05 +0300

    Readding the fixed Infrastructure

---
+++ MusicSystem/Infrastructure/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MusicSystem.Data.Models;
+using MusicSystem.Services.Songs;
+
+namespace MusicSystem.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<Artist, SongArtistServiceModel>();
+
+            this.CreateMap<Song, LatestSongServiceModel>();
+            this.CreateMap<SongInfoServiceModel, SongServiceModel>();
+
+            this.CreateMap<Song, SongServiceModel>()
+                .ForMember(c => c.ArtistName, cfg => cfg.MapFrom(c => c.Artist.Name));
+
+            this.CreateMap<Song, SongInfoServiceModel>()
+                .ForMember(c => c.UserId, cfg => cfg.MapFrom(c => c.Curator.UserId))
+                .ForMember(c => c.ArtistName, cfg => cfg.MapFrom(c => c.Artist.Name));
+        }
+    }
+}
