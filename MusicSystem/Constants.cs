5ad4334536dd0eba532d85ceb7340bd19b9fc787
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-16 21:14:31 +0300

    Updating old files

---
+++ MusicSystem/Constants.cs
@@ -0,0 +1,10 @@
+﻿public class Constants
+{
+    public const string GlobalMessageKey = "GlobalMessage";
+
+    public class Cache
+    {
+        public const string LatestSongsCacheKey = nameof(LatestSongsCacheKey);
+    }
+
+}
--- MusicSystem/Startup.cs
+++ MusicSystem/Startup.cs
@@ -1,41 +1,65 @@
-using MusicSystem.Infrastructure;
+using MusicSystem.Data;
+using MusicSystem.Data.Models;
+using MusicSystem.Services.Songs;
+using MusicSystem.Services.Curators;
+using MusicSystem.Services.Statistics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MusicSystem.Data;
+using MusicSystem.Services;
+using MusicSystem.Infrastructure.Extensions;
+using MusicSystem.Services.Partners;
+using MusicSystem.Services.Artists;
+using MusicSystem.Services.Feedbacks;
 
 namespace MusicSystem
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        => Configuration = configuration;
+        public Startup(IConfiguration configuration) 
+            => this.Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MusicSystemDbContext>(options 
-                => options
-                  .UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
-
+            services
+                .AddDbContext<MusicSystemDbContext>(options => options
+                    .UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services
-                .AddDefaultIdentity<IdentityUser>(options =>
+                .AddDefaultIdentity<User>(options =>
                 {
                     options.Password.RequireDigit = false;
                     options.Password.RequireLowercase = false;
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireUppercase = false;
                 })
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<MusicSystemDbContext>();
 
-            services.AddControllersWithViews();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMemoryCache();
+
+            services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+            });
+
+            services.AddTransient<ISongService, SongService>();
+            services.AddTransient<ICuratorService, CuratorService>();
+            services.AddTransient<IPartnerService, PartnerService>();
+            services.AddTransient<IArtistService, ArtistService>();
+            services.AddTransient<IStatisticsService, StatisticsService>();
+            services.AddTransient<IFeedbackService, FeedbackService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -52,19 +76,20 @@ namespace MusicSystem
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
-            app.UseRouting();
+            app
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapDefaultAreaRoute();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-                endpoints.MapRazorPages();
-            });
+                    endpoints.MapDefaultControllerRoute();
+                    endpoints.MapRazorPages();
+                });
         }
     }
 }
