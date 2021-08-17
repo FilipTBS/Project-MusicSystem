72fc06e967e9ad9994f136fa24baba061703b25c
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-17 15:19:06 +0300

    Changed from Latest songs to Example songs

---
+++ MusicSystem/Infrastructure/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MusicSystem.Data.Models;
+using MusicSystem.Models.Songs;
+using MusicSystem.Services.Songs;
+
+namespace MusicSystem.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<Artist, SongArtistServiceModel>();
+
+            this.CreateMap<Song, ExampleSongServiceModel>();
+            this.CreateMap<SongInfoServiceModel, SongFormModel>();
+
+            this.CreateMap<Song, SongServiceModel>()
+                .ForMember(c => c.ArtistName, cfg => cfg.MapFrom(c => c.Artist.Name));
+
+            this.CreateMap<Song, SongInfoServiceModel>()
+                .ForMember(c => c.UserId, cfg => cfg.MapFrom(c => c.Curator.UserId))
+                .ForMember(c => c.ArtistName, cfg => cfg.MapFrom(c => c.Artist.Name));
+        }
+    }
+}
