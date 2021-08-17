a296c28412e1d40b845cbdbfa7485328e58823f9
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-17 15:17:32 +0300

    Changed from Latest songs to Example songs

---
+++ MusicSystem/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicSystem.Models;
+using System.Diagnostics;
+using System.Linq;
+using MusicSystem.Services.Songs;
+using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
+using System;
+
+namespace MusicSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISongService songs;
+        private readonly IMemoryCache cache;
+
+        public HomeController(ISongService songs, IMemoryCache cache)
+        {
+            this.songs = songs;
+            this.cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            var exampleSongs = this.cache
+                .Get<List<ExampleSongServiceModel>>("ExampleSongsCache");
+            if (exampleSongs == null)
+            {
+                exampleSongs = this.songs.Example().ToList();
+
+                var options = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(10));
+
+                this.cache.Set("ExampleSongsCache", exampleSongs, options);
+            }       
+
+            return View(exampleSongs);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel 
+        { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
+    }
+}
---
+++ MusicSystem/Controllers/PartnersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MusicSystem.Infrastructure.Extensions;
+using MusicSystem.Models.Partners;
+using MusicSystem.Services.Partners;
+using static Constants;
+
+namespace MusicSystem.Controllers
+{
+    public class PartnersController : Controller
+    {
+        private readonly IPartnerService partners;
+
+        public PartnersController(IPartnerService partners)
+        => this.partners = partners;
+
+        [Authorize]
+        public IActionResult Become() => View();
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Become(BecomePartnerFormModel partner)
+        {
+            var userId = this.User.GetId();
+
+            var userIsAlreadyPartner = this.partners.IsPartner(userId);
+                //.Partners.Any(d => d.UserId == userId);
+
+            if (userIsAlreadyPartner)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(partner);
+            }
+
+            this.partners.Add(partner.CompanyName, partner.BusinessEmail, partner.Website, userId);
+
+            TempData[GlobalMessageKey] = "Thank you for becoming a Partner!";
+
+            return RedirectToAction("Add", "Artists");
+        }
+
+    }
+}
