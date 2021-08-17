9cde2268476a84348d88776ff73f4d92a96cf396
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-17 15:13:27 +0300

    Changed Latest to Example songs

---
+++ MusicSystem/Services/Songs/SongArtistServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicSystem.Services.Songs
+{
+    public class SongArtistServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
