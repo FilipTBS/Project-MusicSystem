0ae8a03e1953bf783b0b75cda512480320551d23
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-18 22:29:50 +0300

    Updated Artist models

---
+++ MusicSystem/Models/Artists/ArtistSongsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MusicSystem.Models.Artists
+{
+    public class ArtistSongsViewModel
+    {
+        public ICollection<string> SongTitles { get; set; }
+    }
+}
---
+++ MusicSystem/Models/Artists/DeleteArtistFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static MusicSystem.Data.Constants.Artist;
+
+namespace MusicSystem.Models.Artists
+{
+    public class DeleteArtistFormModel
+    {
+        [Required, StringLength(ArtistNameMaxValue, MinimumLength = ArtistNameMinValue)]
+        public string Name { get; set; }
+    }  
+}
