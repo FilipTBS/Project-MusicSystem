46e92e9d36dde6b47406561cfe42b112197635c9
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-18 14:29:34 +0300

    Models now are in a folder

---
+++ MusicSystem/Services/Artists/IArtistService.cs
@@ -0,0 +1,24 @@
+﻿using MusicSystem.Data.Models;
+using MusicSystem.Models.Artists;
+using System.Collections.Generic;
+
+namespace MusicSystem.Services
+{
+    public interface IArtistService
+    {
+        ArtistQueryServiceModel Catalogue(string artist,
+                                 string searchTerm,
+                                 int currentPage,
+                                 int artistsPerPage);
+
+        public ArtistSongsViewModel GetArtistSongs(string id);
+
+        public bool Exists(string name);
+
+        public string Add(string name, string genre, ICollection<Song> songs);
+
+        public void Delete(string name);
+
+        public Artist FindArtist(string name);
+    }
+}
