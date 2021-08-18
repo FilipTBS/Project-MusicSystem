f2f0c7c57ece08248bf2b630cc5b96bd23e1f134
Filip Obretenov <35190655+FilipTBS@users.noreply.github.com>
2021-08-18 14:27:41 +0300

    Models now in folder

---
+++ MusicSystem/Services/Feedbacks/IFeedbackService.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicSystem.Services.Feedbacks
+{
+    public interface IFeedbackService
+    {
+        public string AddFeedback(string userId,
+           int score, string suggestion);
+
+        public FeedbackQueryServiceModel All();
+
+        public bool UserHasGivenFeedback(string id);
+
+    }
+}
---
+++ MusicSystem/Services/Feedbacks/Models/FeedbackQueryServiceModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MusicSystem.Services.Feedbacks
+{
+    public class FeedbackQueryServiceModel
+    {
+        public int TotalFeedbacks { get; set; }
+
+        public ICollection<FeedbackServiceModel> Feedbacks { get; set; }
+    }
+}
